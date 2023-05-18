9b4f9b871926b997ce5f6fa7f5308b59d77c2e0f
Gameel-Allam <fadyallam8@gmail.com>
2023-05-18 21:24:24 +0300

    -Add new edits after merge

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/main.scss";
+import "./assets/styles/main.scss";
 // import { store } from "./app/store.js";
 // import { Provider } from "react-redux";
 
