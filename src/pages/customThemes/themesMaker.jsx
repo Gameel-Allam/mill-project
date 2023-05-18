4b6056f71f6584fd4fcf9c3fc470179ccf3c2a7c
Mohamed-Mohamed-Afifi <mohamed.org050@gmail.com>
2023-05-18 20:48:45 +0300

    solve megre

--- src/pages/customThemes/themesMaker.jsx
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from '@mui/styles';
-export const useStyles=makeStyles(()=>({
-  textField:{
-      border:"1px solid white",
-      padding:"5px",
-      color:"#ffffff !important",
-  }
-}))
